2cd108e90654b7d7210af8629832d84649eaec94
rktel <rakinakuy@hotmail.com>
2019-07-16 23:08:04 -0500

    button

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,15 +1,16 @@
 <template>
-    <section>
-        <button>Start</button>
-    </section>
+  <section>
+    <div class="frow justify-around">
+      <button class="col-xs-3-10">Regular</button>
+      <button class="col-xs-3-10" disabled>Disabled</button>
+      <a type="button" class="col-xs-3-10">Anchor</a>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
