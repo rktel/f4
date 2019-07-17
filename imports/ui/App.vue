5095bec7cf26367a241e829ae93a7fd5f0b9b92c
rktel <rakinakuy@hotmail.com>
2019-07-17 17:36:56 -0500

    login localStorage 4

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -8,7 +8,7 @@ export default {
     beforeCreate() {
       console.log(localStorage.getItem("user"));
 
-        localStorage.getItem("user") == 1
+        !!localStorage.getItem("user")
             ? this.$router.push({ name: "F4" })
             : this.$router.push({ name: "Login" });
     }
