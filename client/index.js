7c9e439bf3bde4143d258f376244e0774b91763e
rktel <rakinakuy@hotmail.com>
2019-07-16 23:12:32 -0500

    Vue tip off

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+Vue.config.productionTip = false;
 import VueMeteorTracker from 'vue-meteor-tracker';   // here!
 Vue.use(VueMeteorTracker);                           // here!
 
