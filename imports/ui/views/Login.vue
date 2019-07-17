774db0d9d1155bf94192878b984c218dfcc97b3c
rktel <rakinakuy@hotmail.com>
2019-07-17 13:40:05 -0500

    login layout 10

--- imports/ui/views/Login.vue
+++ imports/ui/views/Login.vue
@@ -1,17 +1,30 @@
 <template>
     <section class="flex-container-login">
         <div class="form-login">
-            <button>Login</button>
+            <form class="pure-form pure-form-aligned">
+                <fieldset>
+                    <div class="pure-control-group">
+                        <label for="name">Username</label>
+                        <input id="name" type="text" />
+                    </div>
+
+                    <div class="pure-control-group">
+                        <label for="password">Password</label>
+                        <input id="password" type="password"/>
+                    </div>
+
+                    <div class="pure-controls">
+                        <button type="submit" class="pure-button pure-button-primary">Submit</button>
+                    </div>
+                </fieldset>
+            </form>
         </div>
     </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
