2b68740135e9d60b032e2d64560a24e10174421f
rktel <rakinakuy@hotmail.com>
2019-08-08 01:35:30 -0500

    socket.io 13

--- server/core/socket.io.js
+++ server/core/socket.io.js
@@ -6,11 +6,6 @@ const serverio = io.listen(8000);
 serverio.on("connection", (socket) => {
     console.info(`Client connected [id=${socket.id}]`);
     // initialize this client's sequence number
-    
-    Meteor.call("getAllMobiles", (error, mobiles) => {
-        if (!error) {
-          console.log(mobiles);
-          socket.emit('message', mobiles);
-        }
-      });
+    socket.emit('message', main_getAllMobiles());
+
 });
