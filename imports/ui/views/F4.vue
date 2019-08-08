7c2604b95d4f62101643da8e0990a2a0ad19d077
rktel <rakinakuy@hotmail.com>
2019-08-08 01:01:18 -0500

    socket.io 10

--- imports/ui/views/F4.vue
+++ imports/ui/views/F4.vue
@@ -1,64 +1,64 @@
 <template>
-    <section class="area-f4">
-        <nav>
-            <button class="button-xsmall pure-button" disabled>F4</button>
-            <button class="button-xsmall pure-button" @click="logout">Logout</button>
-        </nav>
-        <section class="content-f4">
-            <div class="item1">ONE</div>
-            <div class="item2">TWO</div>
-        </section>
+  <section class="area-f4">
+    <nav>
+      <button class="button-xsmall pure-button" disabled>F4</button>
+      <button class="button-xsmall pure-button" @click="logout">Logout</button>
+    </nav>
+    <section class="content-f4">
+      <div class="item1">ONE</div>
+      <div class="item2">TWO</div>
     </section>
+  </section>
 </template>
 
 <script>
-import { LOCAL_STORAGE_USERNAME } from '../../api/constants';
+import { LOCAL_STORAGE_USERNAME } from "../../api/constants";
 
 export default {
-    name:'F4',
-    beforeCreate() {
-          socket.on('message', function(message) {
-                alert('The server has a message for you: ' + message);
-            })
-        Meteor.call('getAllMobiles', (error, mobiles)=>{
-            if(!error){
-                console.log(mobiles);                
-            }
-        })
-    },
-    methods: {
-        logout() {
-            localStorage.setItem(LOCAL_STORAGE_USERNAME, 0);
-            this.$router.push({ name: "Login" });
-        }
+  name: "F4",
+  beforeCreate() {
+    socket.on("message", function(message) {
+      alert("The server has a message for you: " + message);
+    });
+    Meteor.call("getAllMobiles", (error, mobiles) => {
+      if (!error) {
+        console.log(mobiles);
+      }
+    });
+  },
+  methods: {
+    logout() {
+      localStorage.setItem(LOCAL_STORAGE_USERNAME, 0);
+      this.$router.push({ name: "Login" });
     }
+  }
 };
 </script>
 
 <style scope>
-.area-f4{
-    height: 100%;
+.area-f4 {
+  height: 100%;
 }
-.area-f4 nav{
-    height: 25px;
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-flex-direction: row;
-    -ms-flex-direction: row;
-    flex-direction: row;
-    -webkit-flex-wrap: wrap;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-    -webkit-justify-content: space-between;
-    -ms-flex-pack: justify;
-    justify-content: space-between;
-    -webkit-align-content: stretch;
-    -ms-flex-line-pack: stretch;
-    align-content: stretch;
-    -webkit-align-items: flex-start;
-    -ms-flex-align: start;
-    align-items: flex-start;
+.area-f4 nav {
+  height: 25px;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: row;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-flex-wrap: wrap;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -webkit-justify-content: space-between;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -webkit-align-content: stretch;
+  -ms-flex-line-pack: stretch;
+  align-content: stretch;
+  -webkit-align-items: flex-start;
+  -ms-flex-align: start;
+  align-items: flex-start;
 }
 .content-f4 {
   /* Subtract the header size */
@@ -68,27 +68,26 @@ export default {
   flex-direction: row;
   align-content: stretch;
 }
-.content-f4 div.item1{
-            flex: 3 1;
-            order: 0;
-            align-self: stretch;
+.content-f4 div.item1 {
+  flex: 3 1;
+  order: 0;
+  align-self: stretch;
 }
-.content-f4 div.item2{
-            flex: 7 1;
-            order: 0;
-            align-self: stretch;
-            background-color: gainsboro;
+.content-f4 div.item2 {
+  flex: 7 1;
+  order: 0;
+  align-self: stretch;
+  background-color: gainsboro;
 }
 @media only screen and (max-width: 900px) {
-    .content-f4 {
-          flex-direction: column;
-    }
-    .content-f4 div.item1 {
-        flex: 1 1 auto;
-    }
-    .content-f4 div.item2{
-        flex: 1 1 auto;
-    }
+  .content-f4 {
+    flex-direction: column;
+  }
+  .content-f4 div.item1 {
+    flex: 1 1 auto;
+  }
+  .content-f4 div.item2 {
+    flex: 1 1 auto;
+  }
 }
-
 </style>
--- server/core/socket.io.js
+++ server/core/socket.io.js
@@ -1,3 +1,5 @@
 //#Socket.io
 //const SOCKET_IO_PORT = 80;
 const io = require('socket.io')();
+
+io.sockets.emit('message', 'You are connected!');
